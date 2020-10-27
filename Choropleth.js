9b3b4c3666ac9244873689bd4a4c5c02db48bb60
Ameya Yerpude <aryerpude@dons.usfca.edu>
2020-10-26 22:50:13 -0700

    First Commit

---
+++ Choropleth.js
@@ -0,0 +1,212 @@
+var svg = d3.select("svg"),
+    width = +svg.attr("width"),
+    height = +svg.attr("height");
+
+var uv = new Map();
+
+var path = d3.geoPath();
+
+
+var color = d3.scaleQuantize([2500,5650], d3.schemeBlues[9]);
+
+
+
+var g = svg.append("g")
+    .attr("class", "key")
+    .attr("transform", "translate(20,20)");
+
+svg
+    .append("g")
+    .attr("transform", "translate(610,20)")
+    .append(() => legend({ color, title: "UV Radiation", width: 260 }));
+
+var promises = [
+        d3.json("https://d3js.org/us-10m.v2.json"),
+        d3.csv("uv-county.csv", function(d) {
+            uv.set(d["COUNTY_FIPS"], +d["UV_Wh/m²"]);
+        })
+    ];
+
+    Promise.all(promises).then(ready);
+
+function ready([us]) {
+    console.log(uv);
+    let mouseOver = function(d) {
+        d3.selectAll(".counties")
+            .transition()
+            .duration(200)
+            .style("opacity", .5);
+        d3.select(this)
+            .transition()
+            .duration(200)
+            .style("opacity", 1)
+            .style("stroke", "black")
+    };
+
+    let mouseLeave = function(d) {
+        d3.selectAll(".counties")
+            .transition()
+            .duration(200)
+            .style("opacity", .8);
+        d3.select(this)
+            .transition()
+            .duration(200)
+            .style("stroke", "transparent")
+    };
+    console.log(topojson.feature(us, us.objects.counties).features);
+    svg.append("g")
+        .attr("class", "counties")
+        .selectAll("path")
+        .data(topojson.feature(us, us.objects.counties).features)
+        .enter().append("path")
+        .attr("fill", function(d) {
+            return color(d.rate = (uv.get(d.id) != undefined ? uv.get(d.id) : 2658));
+        })
+        .attr("d", path)
+        .append("title")
+        .text(function(d) { return d.rate; })
+        .on("mouseover", mouseOver )
+        .on("mouseleave", mouseLeave );
+
+
+    svg.append("path")
+        .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
+        .attr("class", "states")
+        .attr("d", path)
+
+}
+
+function legend({
+                    color,
+                    title,
+                    tickSize = 6,
+                    width = 320,
+                    height = 44 + tickSize,
+                    marginTop = 18,
+                    marginRight = 0,
+                    marginBottom = 16 + tickSize,
+                    marginLeft = 0,
+                    ticks = width / 64,
+                    tickFormat,
+                    tickValues
+                } = {}) {
+
+    const svg = d3.create("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .attr("viewBox", [0, 0, width, height])
+        .style("overflow", "visible")
+        .style("display", "block");
+
+    let tickAdjust = g => g.selectAll(".tick line").attr("y1", marginTop + marginBottom - height);
+    let x;
+
+    // Continuous
+    if (color.interpolate) {
+        const n = Math.min(color.domain().length, color.range().length);
+
+        x = color.copy().rangeRound(d3.quantize(d3.interpolate(marginLeft, width - marginRight), n));
+
+        svg.append("image")
+            .attr("x", marginLeft)
+            .attr("y", marginTop)
+            .attr("width", width - marginLeft - marginRight)
+            .attr("height", height - marginTop - marginBottom)
+            .attr("preserveAspectRatio", "none")
+            .attr("xlink:href", ramp(color.copy().domain(d3.quantize(d3.interpolate(0, 1), n))).toDataURL());
+    }
+
+    // Sequential
+    else if (color.interpolator) {
+        x = Object.assign(color.copy()
+                .interpolator(d3.interpolateRound(marginLeft, width - marginRight)),
+            {range() { return [marginLeft, width - marginRight]; }});
+
+        svg.append("image")
+            .attr("x", marginLeft)
+            .attr("y", marginTop)
+            .attr("width", width - marginLeft - marginRight)
+            .attr("height", height - marginTop - marginBottom)
+            .attr("preserveAspectRatio", "none")
+            .attr("xlink:href", ramp(color.interpolator()).toDataURL());
+
+        // scaleSequentialQuantile doesn’t implement ticks or tickFormat.
+        if (!x.ticks) {
+            if (tickValues === undefined) {
+                const n = Math.round(ticks + 1);
+                tickValues = d3.range(n).map(i => d3.quantile(color.domain(), i / (n - 1)));
+            }
+            if (typeof tickFormat !== "function") {
+                tickFormat = d3.format(tickFormat === undefined ? ",f" : tickFormat);
+            }
+        }
+    }
+
+    // Threshold
+    else if (color.invertExtent) {
+        const thresholds
+            = color.thresholds ? color.thresholds() // scaleQuantize
+            : color.quantiles ? color.quantiles() // scaleQuantile
+                : color.domain(); // scaleThreshold
+
+        const thresholdFormat
+            = tickFormat === undefined ? d => d
+            : typeof tickFormat === "string" ? d3.format(tickFormat)
+                : tickFormat;
+
+        x = d3.scaleLinear()
+            .domain([-1, color.range().length - 1])
+            .rangeRound([marginLeft, width - marginRight]);
+
+        svg.append("g")
+            .selectAll("rect")
+            .data(color.range())
+            .join("rect")
+            .attr("x", (d, i) => x(i - 1))
+            .attr("y", marginTop)
+            .attr("width", (d, i) => x(i) - x(i - 1))
+            .attr("height", height - marginTop - marginBottom)
+            .attr("fill", d => d);
+
+        tickValues = d3.range(thresholds.length);
+        tickFormat = i => thresholdFormat(thresholds[i], i);
+    }
+
+    // Ordinal
+    else {
+        x = d3.scaleBand()
+            .domain(color.domain())
+            .rangeRound([marginLeft, width - marginRight]);
+
+        svg.append("g")
+            .selectAll("rect")
+            .data(color.domain())
+            .join("rect")
+            .attr("x", x)
+            .attr("y", marginTop)
+            .attr("width", Math.max(0, x.bandwidth() - 1))
+            .attr("height", height - marginTop - marginBottom)
+            .attr("fill", color);
+
+        tickAdjust = () => {};
+    }
+
+    svg.append("g")
+        .attr("transform", `translate(0,${height - marginBottom})`)
+        .call(d3.axisBottom(x)
+            .ticks(ticks, typeof tickFormat === "string" ? tickFormat : undefined)
+            .tickFormat(typeof tickFormat === "function" ? tickFormat : undefined)
+            .tickSize(tickSize)
+            .tickValues(tickValues))
+        .call(tickAdjust)
+        .call(g => g.select(".domain").remove())
+        .call(g => g.append("text")
+            .attr("x", marginLeft)
+            .attr("y", marginTop + marginBottom - height - 6)
+            .attr("fill", "currentColor")
+            .attr("text-anchor", "start")
+            .attr("font-weight", "bold")
+            .text(title));
+
+    return svg.node();
+}
